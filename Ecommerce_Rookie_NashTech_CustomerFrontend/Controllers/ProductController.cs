2b4b66edcf93dda87f92ed4a28c20cfd5c227862
QuanggDat <datqit@gmail.com>
2024-05-23 17:32:38 +0700

    add Customer Frontend

---
+++ Ecommerce_Rookie_NashTech_CustomerFrontend/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Rookie_NashTech_CustomerFrontend.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ecommerce_Rookie_NashTech_CustomerFrontend/ViewComponents/MenuCategoryViewComponent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Rookie_NashTech_CustomerFrontend.ViewComponents
+{
+    public class MenuCategoryViewComponent : ViewComponent
+    {
+    }
+}
